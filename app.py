4216a8d4790ef899b0d265f28353a924e512a2c1
G. Ayyappa Venkata Sai <162156423+ayyappa2006@users.noreply.github.com>
2026-04-07 10:34:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import pickle
 import os
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 
-# Correct path for Render
 model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
 model = pickle.load(open(model_path, 'rb'))
 
+# LOGIN
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] == 'admin' and request.form['password'] == '1234':
+            session['user'] = 'admin'
+            return redirect(url_for('home'))
+        else:
+            return "Invalid Login"
+
+    return render_template('login.html')
+
+# HOME (Protected)
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if 'user' in session:
+        return render_template('index.html')
+    return redirect(url_for('login'))
 
+# PREDICTION
 @app.route('/predict', methods=['POST'])
 def predict():
     study_hours = float(request.form['study_hours'])
